d953b0ac75e6b0218e8d5943a707eb6c92ee206d
LENOVO <LENOVO@DESKTOP-0089CLJ>
2020-06-27 13:13:41 +0300

    A little update for Exercise 2

--- JavaExercises/src/exercises1/Exercise2.java
+++ JavaExercises/src/exercises1/Exercise2.java
@@ -16,36 +16,26 @@ public class Exercise2 {
 	public static int[] rotate (int[] arr, int toLeft) {
 		if (toLeft>arr.length) {
 			toLeft = toLeft%arr.length;
-			//System.out.println("toLeft =" + toLeft);
 		} else if (toLeft<0) {
 			if (-1*toLeft>arr.length) {
 				toLeft = toLeft%arr.length;
-				//System.out.println("toLeft =" + toLeft);
 			}
 			toLeft=toLeft+arr.length;
-			//System.out.println("toLeft =" + toLeft);
 		}
 		int[] res = new int[arr.length];
 		int[] accumulate1 = new int[arr.length-toLeft];
 		accumulate1 = Arrays.copyOfRange(arr, toLeft, arr.length);
 		int[] accumulate2 = new int[toLeft];
 		accumulate2 = Arrays.copyOfRange(arr, 0, toLeft);
-        for (int i : accumulate1) 
-            System.out.print(i + "  ");
         System.out.println();
-        for (int i : accumulate2) 
-            System.out.print(i + "  ");
-        System.out.println();
-        System.arraycopy(accumulate1, 0, res, 0, accumulate1.length);  
-        System.arraycopy(accumulate2, 0, res, accumulate1.length, accumulate2.length);
-        for (int i : res) 
-            System.out.print(i + "  ");        
-		return res;
+        System.arraycopy(accumulate1, 0, arr, 0, accumulate1.length);  
+        System.arraycopy(accumulate2, 0, arr, accumulate1.length, accumulate2.length);       
+		return arr;
 	}
 	
 	public static void main(String[] args) {
 	    int[] sourceArr1 = {1, 2, 3, 4, 5, 6};
-	    rotate(sourceArr1, -8);
+	    rotate(sourceArr1, 8);
 	}
 
 }
